87a415ef9aa41ab7321503923189e7c09a5342b4
marcelo90fffcc <129770947+marcelo90fffcc@users.noreply.github.com>
2023-10-09 11:40:30 -0300

    Add files via upload

--- marcelomochila-de-viagem/js/main.js
+++ marcelomochila-de-viagem/js/main.js
@@ -1,8 +1,21 @@
 const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
 
-form.addEventlistener("submit", () => {
-    evento.preventDefault();
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault()
 
-    console.log(evento);
-    console.log(evento.target[0].value);
-});
+    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+})
+
+function criaElemento(nome, quantidade) {
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = quantidade
+
+    novoItem.appendChild(numeroItem)
+    novoItem.innerHTML += nome
+
+    lista.appendChild(novoItem)
+}
